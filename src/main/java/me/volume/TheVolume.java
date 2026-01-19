5c20203b7f187d1c78cfd15c712b65585a89a396
i66884004-web <i66884004@gmail.com>
2026-01-19 18:50:29 -0500

    Implement basic plugin lifecycle methods

--- src/main/java/me/volume/TheVolume.java
+++ src/main/java/me/volume/TheVolume.java
@@ -1,1 +1,15 @@
+package me.volume;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TheVolume extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("TheVolume plugin enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("TheVolume plugin disabled!");
+    }
+}
